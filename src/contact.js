1a959d4bcf06cf8ba97aca123409c0371a5e9921
ftrbnd <giosalas25@gmail.com>
2023-03-08 13:34:26 -0800

    Set up tab headers

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,14 @@
 export default function loadContactInfo() {
+    const content = document.querySelector('div#content');
 
+    const header = document.createElement('div');
+    header.classList.add('header');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Contact Us';
+    header.appendChild(h1);
+
+
+
+    content.appendChild(header);
 }
--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,14 @@
 export default function loadMenu() {
+    const content = document.querySelector('div#content');
 
+    const header = document.createElement('div');
+    header.classList.add('header');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Menu';
+    header.appendChild(h1);
+
+
+
+    content.appendChild(header);
 }
